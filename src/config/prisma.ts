0f958aa03c6dcac916efef92e4bb1a7fc0f11649
Devanshu Gound <dewanshugound@gmail.com>
2026-03-27 19:57:53 +0530

    fix: explicitly provide datasources and logs to PrismaClient constructor

--- src/config/prisma.ts
+++ src/config/prisma.ts
@@ -1,3 +1,12 @@
 import { PrismaClient } from '@prisma/client';
 
-export const prisma = new PrismaClient();
+const databaseUrl = process.env.DATABASE_URL || "postgresql://postgres:dummy@localhost:5432/dummy";
+
+export const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url: databaseUrl,
+    },
+  },
+  log: ['query', 'info', 'warn', 'error'],
+});
